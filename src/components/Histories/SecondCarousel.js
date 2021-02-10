ba1c7c6cb2c1830fcec985035f54ab65b7d398df
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-02-10 17:54:23 +0100

    Second Carousel done. Some semicolons need to be removed but it's now totally running properly

--- src/components/Histories/SecondCarousel.js
+++ src/components/Histories/SecondCarousel.js
@@ -2,20 +2,35 @@ import React, { useState } from 'react';
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai';
 
 import { SecondCarouselData } from './SecondCarouselData';
+import './SecondCarousel.css';
 
 const SecondCarousel = ({ slides }) => {
     const [current, setCurrent] = useState(0);
     const length = slides.length;
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
 
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current -1);
+    };
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    };
 
     return (
         <section className="slider">
-            <AiOutlineArrowLeft className="left-arrow" />
-            <AiOutlineArrowRight className="right-arrow" />
+            <AiOutlineArrowLeft className="left-arrow" onClick={prevSlide} />
+            <AiOutlineArrowRight className="right-arrow" onClick={nextSlide} />
             {SecondCarouselData.map((slide, index) => {
                 return (
-                    <img src={slide.image} alt="Gathering image" />
+                    <div className={index === current ? 'slide active' : 'slide'} key={index}>
+                        {index === current && (
+                            <img src={slide.image} alt="Gathering image" className="image" />
+                        )};
+                    </div>
                 )
             })};
         </section>
